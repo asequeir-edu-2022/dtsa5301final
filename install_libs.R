58de867f2e680f0908cef4220a977a7e17733d58
Antony Sequeira <antony.sequeira@colorado.edu>
2022-02-09 18:43:52 -0800

    install script

---
+++ install_libs.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+r = getOption("repos")
+r["CRAN"] = "http://cran.us.r-project.org"
+options(repos = r)
+
+print("Installing R libraries")
+install.packages("chron")
+install.packages("tidyverse")
+install.packages("tinytex")
